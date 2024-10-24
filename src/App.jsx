0955a1265f187183b60ad45b7486a34d042e4f8e
Kevin Andrés <andres200127@hotmail.com>
2024-10-23 21:17:58 -0500

    APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './css/index.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import Dashboard from './components/panel/dashboard'
+import PanelSign from './components/sign/sign'
+import * as actions from './components/store/action/action';
+import { useDispatch, useSelector } from 'react-redux'
+import PanelDashboard from './components/panel/panelDashboard'
 
 function App() {
+  const user = useSelector(store => store.usuario);
+  const loadingUser = useSelector(store => store.loadingUser)
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    const log = JSON.parse(window.localStorage.getItem("loggedPeople"));
+    if(log && !user){
+        dispatch(actions.AxiosAuthUser(log, true));
+    }
+  }, []);
   return (
     <div className="d">
       <Router>
-          <Routes>
-            <Route path="/" element={<h1>Bienvenido</h1>} />
-            <Route path="/panel/*" element={<Dashboard />} />
-          </Routes>
+            <Routes>
+              <Route path="/" element={<h1>Bienvenido</h1>} />
+              <Route path="/sign/*" element={<PanelSign />} />
+              <Route path="/panel/*" element={<PanelDashboard /> } />
+            </Routes>
       </Router>
     </div>
   )
